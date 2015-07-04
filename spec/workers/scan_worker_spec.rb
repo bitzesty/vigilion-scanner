7d2e53e8813125e73f1c0341d89937960a8ad0e3
Emiliano Ritiro <eritiro@gmail.com>
2015-07-04 12:44:29 -0300

    integration tests with EICAR

--- spec/workers/scan_worker_spec.rb
+++ spec/workers/scan_worker_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe ScanWorker do
       ScanWorker.new.perform(nil, "id" => scan.id)
     end
   end
+
+  describe "integration" do
+    context "scan eicar file" do
+      let(:scan) { create(:scan, file: OpenStruct.new(read: "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*")) }
+
+      it "set scan as infected" do
+        ScanWorker.new.perform(nil, "id" => scan.id)
+        expect(scan.reload).to be_infected
+      end
+    end
+
+    context "scan regular file" do
+      let(:scan) { create(:scan, file: OpenStruct.new(read: "regular file")) }
+
+      it "set scan as clean" do
+        ScanWorker.new.perform(nil, "id" => scan.id)
+        expect(scan.reload).to be_clean
+      end
+    end
+  end
 end
