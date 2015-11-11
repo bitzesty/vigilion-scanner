666576e01d6796deeac5a9ad68a28f3a1845612f
Matthew Ford <matt@bitzesty.com>
2015-11-11 11:42:01 +0000

    plan names and lets up the dev plan to 0.5mb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 Plan.update_all available_for_new_subscriptions: false
 
 # free plan
-Plan.create! scans_per_month:   100, cost:    0, file_size_limit: 0.2, name: "Free"
+Plan.create! scans_per_month:   100, cost:    0, file_size_limit: 0.5, name: "developer"
 
 #paid plans
-Plan.create! scans_per_month:  1000, cost:   75
-Plan.create! scans_per_month:  2500, cost:  150
-Plan.create! scans_per_month:  5000, cost:  300
-Plan.create! scans_per_month: 10000, cost:  600
-Plan.create! scans_per_month: 20000, cost: 1000
-Plan.create! scans_per_month: 40000, cost: 2000
+Plan.create! scans_per_month:  1000, cost:   75, name: "1,000"
+Plan.create! scans_per_month:  2500, cost:  150, name: "2,500"
+Plan.create! scans_per_month:  5000, cost:  300, name: "5,000"
+Plan.create! scans_per_month: 10000, cost:  600, name: "10,000"
+Plan.create! scans_per_month: 20000, cost: 1000, name: "20,000"
+Plan.create! scans_per_month: 40000, cost: 2000, name: "40,000"
 
 # special unlimited plan for QAE
 Plan.create! name: "alpha-testers", cost: 0, available_for_new_subscriptions: false
