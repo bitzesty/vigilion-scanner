c48a8124d5dfa8f950d4d840c0fd30d46b4bbdf5
Matthew Ford <matt@bitzesty.com>
2015-12-05 01:31:40 +0000

    logs are timestamped already in papertrail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,6 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.lograge.enabled = true
-  # add time to lograge
-  config.lograge.custom_options = lambda do |event|
-    { time: event.time }
-  end
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
