eaadbe5b9f74ff531874d93a45cb75a94f3e6bcf
Matthew Ford <matt@bitzesty.com>
2016-05-20 18:30:48 +0100

    fix specs

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe AccountsController, type: :controller do
     describe "GET #show" do
       it "assigns the requested account as @account" do
         account = Account.create! valid_attributes
-        get :show, {:id => account.to_param}
+        get :show, params: { id: account.to_param }
         expect(assigns(:account)).to eq(account)
       end
     end
@@ -23,12 +23,12 @@ RSpec.describe AccountsController, type: :controller do
       context "with valid params" do
         it "creates a new Account" do
           expect {
-            post :create, {:account => valid_attributes}
+            post :create, params: { account: valid_attributes }
           }.to change(Account, :count).by(1)
         end
 
         it "assigns a newly created account as @account" do
-          post :create, {:account => valid_attributes}
+          post :create, params: { account: valid_attributes }
           expect(assigns(:account)).to be_a(Account)
           expect(assigns(:account).plan).to eq(plan)
           expect(assigns(:account)).to be_enabled
@@ -36,20 +36,20 @@ RSpec.describe AccountsController, type: :controller do
         end
 
         it "shows the created account" do
-          post :create, {:account => valid_attributes}
+          post :create, params: { account: valid_attributes }
           expect(response).to be_created
         end
       end
 
       context "with invalid params" do
         it "assigns a newly created but unsaved account as @account" do
-          post :create, {:account => invalid_attributes}
+          post :create, params: { account: invalid_attributes }
           expect(assigns(:account)).to be_a_new(Account)
           expect(assigns(:account)).not_to be_persisted
         end
 
         it "returns unprocessable_entity" do
-          post :create, {:account => invalid_attributes}
+          post :create, params: { account: invalid_attributes }
           expect(response.status).to eq(422)
         end
       end
@@ -61,20 +61,20 @@ RSpec.describe AccountsController, type: :controller do
 
         it "updates the requested account" do
           account = Account.create! valid_attributes
-          put :update, {:id => account.to_param, :account => new_attributes}
+          put :update, params: { id: account.to_param, account: new_attributes }
           account.reload
           expect(account).not_to be_enabled
         end
 
         it "assigns the requested account as @account" do
           account = Account.create! valid_attributes
-          put :update, {:id => account.to_param, :account => valid_attributes}
+          put :update, params: { id: account.to_param, account: valid_attributes }
           expect(assigns(:account)).to eq(account)
         end
 
         it "shows the updated account" do
           account = Account.create! valid_attributes
-          put :update, {:id => account.to_param, :account => valid_attributes}
+          put :update, params: { id: account.to_param, account: valid_attributes }
           expect(response).to be_ok
         end
       end
@@ -82,13 +82,13 @@ RSpec.describe AccountsController, type: :controller do
       context "with invalid params" do
         it "assigns the account as @account" do
           account = Account.create! valid_attributes
-          put :update, {:id => account.to_param, :account => invalid_attributes}
+          put :update, params: { id: account.to_param, account: invalid_attributes }
           expect(assigns(:account)).to eq(account)
         end
 
         it "returns unprocessable_entity" do
           account = Account.create! valid_attributes
-          put :update, {:id => account.to_param, :account => invalid_attributes}
+          put :update, params: { id: account.to_param, account: invalid_attributes }
           expect(response.status).to eq(422)
         end
       end
@@ -98,13 +98,13 @@ RSpec.describe AccountsController, type: :controller do
       it "destroys the requested account" do
         account = Account.create! valid_attributes
         expect {
-          delete :destroy, {:id => account.to_param}
+          delete :destroy, params: { id: account.to_param }
         }.to change(Account, :count).by(-1)
       end
 
       it "returns no content" do
         account = Account.create! valid_attributes
-        delete :destroy, {:id => account.to_param}
+        delete :destroy, params: { id: account.to_param }
         expect(response.status).to be(204)
       end
     end
