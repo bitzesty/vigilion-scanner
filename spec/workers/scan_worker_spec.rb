1968bea8aa026ddf7c055f7d965390393d451efe
Matthew Ford <matt@bitzesty.com>
2016-06-15 18:09:12 +0100

    debugging

--- spec/workers/scan_worker_spec.rb
+++ spec/workers/scan_worker_spec.rb
@@ -11,18 +11,18 @@ RSpec.describe ScanWorker do
 
   describe "integration" do
     context "scan eicar file" do
-      let(:scan) { create(:scan, file: OpenStruct.new(read: "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*")) }
-
       it "set scan as infected" do
+        scan = create(:scan, file: OpenStruct.new(read: "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"))
+        puts scan.id
         ScanWorker.new.perform(scan.id)
         expect(scan.reload).to be_infected
       end
     end
 
     context "scan regular file" do
-      let(:scan) { create(:scan, file: OpenStruct.new(read: "regular file")) }
-
       it "set scan as clean" do
+        scan = create(:scan, file: OpenStruct.new(read: "regular file"))
+        puts scan.id
         ScanWorker.new.perform(scan.id)
         expect(scan.reload).to be_clean
       end
