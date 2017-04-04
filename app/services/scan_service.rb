69ed52352122cfd2c9bcfbc6da8a38c14dab9da2
Mario Andrés Correa <a@macool.me>
2017-04-04 00:21:45 -0500

    raise scanning exceptions
    stop sidekiq from swallowing exceptions and therefore jobs. Raise exceptions so these jobs are captured by sentry and retried later

--- app/services/scan_service.rb
+++ app/services/scan_service.rb
@@ -1,17 +1,12 @@
 class ScanService
   def perform(scan)
     return unless scan.pending?
-    begin
-      scan.start!
-      if FileDownloader.new.download scan
-        AvRunner.new.perform scan
-      end
-    rescue => ex
-      Sidekiq.logger.error ex
-      Sidekiq.logger.error ex.backtrace.join("\n")
-    ensure
-      scan.delete_file
-      ClientNotifier.new.notify scan
+
+    scan.start!
+    if FileDownloader.new.download scan
+      AvRunner.new.perform scan
     end
+    scan.delete_file
+    ClientNotifier.new.notify scan
   end
 end
