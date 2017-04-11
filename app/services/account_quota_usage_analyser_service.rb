52e60d22eac6454b26e7df1ff88f0993fac3e02e
Mario Andrés Correa <a@macool.me>
2017-04-11 17:26:48 -0500

    address scenario where plan has no monthly limit

--- app/services/account_quota_usage_analyser_service.rb
+++ app/services/account_quota_usage_analyser_service.rb
@@ -8,9 +8,16 @@ class AccountQuotaUsageAnalyserService
   end
 
   def perform!
+    return if unlimited_plan?
     alert_amount = @plan.scans_per_month * QUOTA_ALERT
     if @account.scan_amount_this_month == alert_amount.to_i
       AccountQuotaUsageMailer.alert_support(@scan).deliver_later
     end
+  end
+
+  private
+
+  def unlimited_plan?
+    @plan.scans_per_month.nil?
   end
 end
