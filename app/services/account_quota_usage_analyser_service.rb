1fe5ba16ca844eb047ffeb2a78e146cefa237b5c
Mario Andrés Correa <a@macool.me>
2017-04-13 08:16:34 -0500

    alert account’s email address on 90% quota usage
    - add `name` and `alert_email` to account so we can deliver an email alert notification and we use the name to identify the accounts

--- app/services/account_quota_usage_analyser_service.rb
+++ app/services/account_quota_usage_analyser_service.rb
@@ -7,7 +7,8 @@ class AccountQuotaUsageAnalyserService
     end
   end
 
-  QUOTA_ALERT = 0.8 # 80%
+  QUOTA_CLIENT_ALERT = 0.9 # 90%
+  QUOTA_SUPPORT_ALERT = 0.8 # 80%
 
   def initialize(account:)
     @account = account
@@ -16,13 +17,26 @@ class AccountQuotaUsageAnalyserService
 
   def perform!
     return if unlimited_plan?
-    alert_amount = @plan.scans_per_month * QUOTA_ALERT
-    if @account.scan_amount_this_month >= alert_amount.to_i
+    alert_support!
+    alert_client!
+  end
+
+  private
+
+  def alert_support!
+    support_alert_amount = @plan.scans_per_month * QUOTA_SUPPORT_ALERT
+    if @account.scan_amount_this_month >= support_alert_amount.to_i
       AccountQuotaUsageMailer.alert_support(@account).deliver_later
     end
   end
 
-  private
+  def alert_client!
+    return if @account.alert_email.blank?
+    client_alert_amount = @plan.scans_per_month * QUOTA_CLIENT_ALERT
+    if @account.scan_amount_this_month >= client_alert_amount.to_i
+      AccountQuotaUsageMailer.alert_client(@account).deliver_later
+    end
+  end
 
   def unlimited_plan?
     @plan.scans_per_month.nil?
--- spec/services/account_quota_usage_analyser_service_spec.rb
+++ spec/services/account_quota_usage_analyser_service_spec.rb
@@ -16,13 +16,35 @@ RSpec.describe AccountQuotaUsageAnalyserService do
     end
 
     context "when 80% usage is hit" do
-      before { create_list :scan, 8, project: account_project }
+      before do
+        ActionMailer::Base.deliveries.clear
+        create_list :scan, 8, project: account_project
+      end
 
-      it "an alert is triggered" do
+      it "one alert is triggered for support" do
         described_class.new(account: account).perform!
         email = ActionMailer::Base.deliveries.last
-        expect(email.subject).to include(account.id.to_s)
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+        expect(email.to).to eq(%w(support@vigilion.com))
+        expect(email.subject).to include(account.name)
         expect(email.body).to include(account_project.name)
+      end
+    end
+
+    context "when 90% usage is hit" do
+      before do
+        ActionMailer::Base.deliveries.clear
+        create_list :scan, 9, project: account_project
+      end
+
+      it "we alert the client **as well**" do
+        described_class.new(account: account).perform!
+        email = ActionMailer::Base.deliveries.last
+        expect(ActionMailer::Base.deliveries.count).to eq(2)
+        expect(email.to).to eq([account.alert_email])
+        expect(email.subject).to include(account.name)
+        expect(email.body).to include(account.name)
+        expect(email.body).to include("limit of #{plan.scans_per_month}")
       end
     end
   end
