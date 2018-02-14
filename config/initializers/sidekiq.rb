ba2dc0c34c34db487a96c8faf633d027a6ec09ac
Mario Andrés Correa <a@macool.me>
2018-02-14 11:53:33 -0500

    do not use logstash formatter for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,6 @@ url = ENV['REDIS_URL'].present? ? ENV['REDIS_URL'] : "redis://:#{ENV['REDIS_ENV_
 
 Sidekiq.configure_server do |config|
   Rails.logger = Sidekiq::Logging.logger
-  config.logger.formatter = Lograge::Formatters::Logstash.new
 
   config.redis = { url: url }
 end
