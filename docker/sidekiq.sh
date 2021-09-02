70be425d5fb4afade03329913279009fd1d26f1f
Matthew Ford <matt@bitzesty.com>
2021-09-02 15:47:30 +0100

    disable avg for now

--- docker/sidekiq.sh
+++ docker/sidekiq.sh
@@ -4,7 +4,7 @@
 nohup clamd &
 
 # boot avg in background
-nohup service avgd start &
+#nohup service avgd start &
 
 # run sidekiq
 cd /usr/src/app && exec bundle exec sidekiq -q default -c 2 >>/var/log/sidekiq.log 2>&1
