40a1c26801acc71f5fef4d10a7a2c0b6d8df682e
Matthew Ford <matt@bitzesty.com>
2024-05-02 17:52:38 +0100

    Use http://www.eicar.com/eicar.com.txt

--- spec/factories/scans.rb
+++ spec/factories/scans.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :scan do
-    url "https://s3.amazonaws.com/vigilion-load-test/eicar.com"
+    url "http://www.eicar.com/eicar.com.txt"
     key "file"
     project
   end
